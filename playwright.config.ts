2ed7517730e85ebd42f3ecca1795d46e2e63ddff
Monami <monami.saeki@gmail.com>
2026-07-29 08:41:49 +0300

    refactor(ci): switch to manually starting Next.js server to stop Playwright from hanging

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  webServer: {
-    command: "npm run start",
-    url: "http://127.0.0.1:3000",
-    reuseExistingServer: true,
-    timeout: 120_000,
-  },
   use: {
     baseURL: "http://127.0.0.1:3000",
   },
