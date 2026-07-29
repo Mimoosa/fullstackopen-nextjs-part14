24e94afd35d5c8337d5c3f6ac0e87c01b87793c0
Monami <monami.saeki@gmail.com>
2026-07-29 08:07:44 +0300

    fix(ci): use npm start

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   webServer: {
-    command: "npm run serve",
+    command: "npm start",
     port: 3000,
     timeout: 120000,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
   },
   use: {
     baseURL: "http://localhost:3000",
